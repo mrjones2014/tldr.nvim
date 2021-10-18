62db724d7efd7437dcf7638973b7f26f139515ba
Mat Jones <mat@mjones.network>
2021-10-18 09:11:51 -0400

    register checkhealth function with telescope

--- lua/telescope/_extensions/tldr.lua
+++ lua/telescope/_extensions/tldr.lua
@@ -2,6 +2,9 @@ return require('telescope').register_extension({
   setup = function(config)
     require('tldr').setup(config)
   end,
+  health = function()
+    require('tldr').health()
+  end,
   exports = {
     tldr = function()
       require('tldr').pick()
