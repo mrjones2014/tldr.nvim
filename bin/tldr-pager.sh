9c8d647e1c16746eb0f22ecdf68f98a7a8b08066
Mat Jones <mat@mjones.network>
2022-01-29 12:26:45 -0500

    add back -R flag for less

--- bin/tldr-pager.sh
+++ bin/tldr-pager.sh
@@ -5,7 +5,7 @@ TLDR_EXTRA_ARGS="$2"
 TLDR_QUERY="$3"
 
 if command -v less &>/dev/null; then
-  eval "$TLDR_COMMAND $TLDR_EXTRA_ARGS $TLDR_QUERY | less"
+  eval "$TLDR_COMMAND $TLDR_EXTRA_ARGS $TLDR_QUERY | less -R"
 else
   eval "$TLDR_COMMAND $TLDR_EXTRA_ARGS $TLDR_QUERY"
 fi
